6be627bdef2c0dd5bbbaf01358db0d52b48e6ea0
Pavneet123 <singhpavneet0312@gmail.com>
2025-11-10 08:58:29 +0530

    add new files

---
+++ src/functions/Student.java
@@ -0,0 +1,17 @@
+package functions;
+
+public class Student {
+    // c-student
+    // A- Attribute |data member | instance varia
+    long phone;
+    String email;
+    int batch8x;
+    String name;
+    boolean isMale;
+    String Address;
+    int age;
+    //behaviour | methods | member functions
+    void study(){} // No Return and No Argument
+    void eat(){}
+    void walk(){}
+}
--- src/functions/lab04.java
+++ src/functions/lab04.java
@@ -1,4 +1,24 @@
 package functions;
 
 public class lab04 {
+    public static void main(String[] args) {
+        //today task
+        // object class --- OOps
+        //string class
+        // Arrays
+            //for each
+            // 2d nested for loop
+
+
+        // class---> blueprint to create something
+        // objects -instance of the class
+
+        Student amit =new Student();
+        amit.age=35;
+
+        Student pavneet=new Student();
+
+        // age - ref variable - byte 8, 64 bit  null default s
+
+    }
 }
